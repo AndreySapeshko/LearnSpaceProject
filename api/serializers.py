7758584ee966b59126c17f3aa38078b4cf1ae67b
Andrey Sapeshko <andrdd17@gmail.com>
2025-11-03 15:14:51 +0300

    В CourseSerializer добавлен вывод информации по всем урокам курса

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from courses.models import Course, Lesson
 
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
     count_lessons = serializers.SerializerMethodField()
     def get_count_lessons(self, obj):
         return Lesson.objects.filter(course=obj.id).count()
 
     class Meta:
         model = Course
-        fields = ['name', 'description', 'preview', 'count_lessons']
+        fields = ['name', 'description', 'preview', 'count_lessons', 'lessons']
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+
