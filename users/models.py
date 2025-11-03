a2fde128f8c3462b649088c4f72e8023d806978c
Andrey Sapeshko <andrdd17@gmail.com>
2025-11-03 09:25:12 +0300

    Изменена модель CustomUser и проведена миграция

--- users/models.py
+++ users/models.py
@@ -4,13 +4,18 @@ from django.db import models
 class CustomUser(AbstractUser):
     """ Класс описывающий модель пользователя """
 
+    username = None
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatar/', blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     country = models.CharField(max_length=70, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
