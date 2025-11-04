8b9854f173d26bd430024181253ba0815350db70
Andrey Sapeshko <andrdd17@gmail.com>
2025-11-04 08:38:09 +0300

    созданы CRUD представления для пользователей и ограничен доступ для не авторизованных пользователей ко всем представлениям кроме регистрации и авторизации

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,16 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('list/', UserListAPIView.as_view(), name='user_list'),
+    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('user/<int:pk>/edit/', UserUpdateAPIView.as_view(), name='user_edit'),
+    path('user/<int:pk>/delete/', UserUpdateAPIView.as_view(), name='user_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,44 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import (
+    CreateAPIView, ListAPIView,
+    RetrieveAPIView, UpdateAPIView, DestroyAPIView
+)
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+
+from users.serializers import UserSerializer
+
+User = get_user_model()
 
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
