b64bbf5ca89384f10dac3a0e1569c6b51e6dac4d
Andrey Sapeshko <andrdd17@gmail.com>
2025-11-12 11:12:44 +0300

    Создана периодическая задача remove_inactive_users

--- courses/tasks.py
+++ courses/tasks.py
@@ -1,8 +1,14 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from datetime import timedelta
 
 from courses.models import Course
+
+User = get_user_model()
+
 
 
 @shared_task
@@ -18,3 +24,13 @@ def course_update_notice(course_id, users_email):
         #     from_email=from_email,
         #     recipient_list=users_email
         # )
+
+@shared_task
+def remove_inactive_users():
+    users = User.objects.filter(is_active=True)
+    thirty_days_ago = timezone.now() - timedelta(days=30)
+    for user in users:
+        if user.last_login < thirty_days_ago:
+            user.is_active = False
+            user.save()
+
